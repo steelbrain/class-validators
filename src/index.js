8d1694717358184bd30e7b16f3e4178e3a7603eb
Steel Brain <i@steelbrain.me>
2018-02-18 09:52:30 +0500

    :new: Add method and generic validator

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
 // @flow
+
+import validateIndividual from './validateIndividual'
+import * as t from './types'
+
+// NOTE: We don't validate any extra args
+function validate(
+  types: Array<any>,
+  values: Array<any>,
+  method: ?string = null,
+  className: ?string = null,
+  aliases: Array<string> = [],
+) {
+  let prefix = method ? `${method}() expects` : 'expected'
+  if (className) {
+    prefix = `${className}#${prefix}`
+  }
+
+  types.forEach(function(type, index) {
+    const value = values[index]
+    const alias = aliases[index] || `Parameter ${index + 1}`
+    try {
+      validateIndividual(type, value, index, types, values, false)
+    } catch (error) {
+      error.message = `${prefix} ${alias}${error.message}`
+      throw error
+    }
+  })
+}
+
+function validateMethod(...types: Array<any>) {
+  return function(target: Object, key: string, descriptor: Object) {
+    const className = target.constructor.name || 'Object'
+    validate([Function], [descriptor.value], 'validateMethod', null, [`${className}#${key}`])
+
+    const realCallback = descriptor.value
+    const validationCallback = function(...values) {
+      validate(types, values, key, className)
+      return realCallback(...values)
+    }
+    Object.setPrototypeOf(validationCallback.prototype, realCallback.prototype)
+
+    // eslint-disable-next-line no-param-reassign
+    descriptor.value = validationCallback
+
+    return descriptor
+  }
+}
+
+export { validate, validateMethod, t }
---
+++ src/validateIndividual.js
@@ -0,0 +1,124 @@
+// @flow
+
+const primitiveTypes = new Map([
+  [String, 'string'],
+  [Number, 'number'],
+  [Boolean, 'boolean'],
+  [Symbol, 'symbol'],
+  [Function, 'function'],
+])
+
+function getPrintableType(type: any) {
+  let prefix = ''
+  let postfix = ''
+  if (type && type.type) {
+    switch (type.type) {
+      case 'var-args':
+        postfix = '...'
+        break
+      case 'optional':
+        prefix = '?'
+        break
+      case 'any':
+        return 'any'
+      default:
+    }
+  }
+
+  if (type.of) {
+    return prefix + getPrintableType(type.of) + postfix
+  }
+  if (typeof type === 'string') {
+    return prefix + type + postfix
+  }
+  if (typeof type === 'object') {
+    return prefix + JSON.stringify(type, (key, value) => getPrintableType(value)) + postfix
+  }
+  return prefix + type.name + postfix
+}
+
+export default function validateIndividual(
+  givenType: any,
+  value: any,
+  index: number,
+  types: Array<any>,
+  values: Array<any>,
+  isObjectProperty: boolean,
+): void {
+  let type = givenType
+  if (value === null || typeof value === 'undefined') {
+    if (type.type === 'optional' || type.type === 'any') {
+      return
+    }
+    throw new Error(` to be non-null ${getPrintableType(type)}`)
+  }
+
+  if (type.type === 'optional') type = type.of
+  if (type.type === 'any') return
+  if (type.type === 'var-args') {
+    if (isObjectProperty) {
+      throw new Error(' to have variadic inside Object property')
+    }
+    if (index !== types.length - 1) {
+      throw new Error(` to be last type. Got: ${index + 1} instead of ${types.length - 1}`)
+    }
+    try {
+      values
+        .slice(index)
+        .every((item, itemIndex) => validateIndividual(type, item, itemIndex, types, values, isObjectProperty))
+    } catch (error) {
+      error.message = `+ ${error.mesage}`
+      throw error
+    }
+    return
+  }
+  if (type.type === 'instance') {
+    if (!(value instanceof type.of)) {
+      throw new Error(
+        ` to be an instance of ${getPrintableType(type)}. Got: ${
+          value ? value.name || value.constructor.name : typeof value
+        }`,
+      )
+    }
+    return
+  }
+  if (type && type.constructor.name === 'Object') {
+    if (!value || typeof value !== 'object') {
+      throw new Error(` to be ${getPrintableType(type)}. Got: ${typeof value}`)
+    }
+
+    Object.keys(type).forEach(function(key) {
+      try {
+        validateIndividual(type[key], value[key], index, types, values, true)
+      } catch (error) {
+        error.message = `[${JSON.stringify(key)}]${error.message}`
+        throw error
+      }
+    })
+    return
+  }
+  if (Array.isArray(type)) {
+    if (!value || !Array.isArray(value)) {
+      throw new Error(` to be ${getPrintableType(type)}`)
+    }
+    type.forEach((item, itemIndex) => {
+      try {
+        validateIndividual(item, value[index], itemIndex, type, value, false)
+      } catch (error) {
+        error.message = `[${index}]${error.message}`
+        throw error
+      }
+    })
+    return
+  }
+
+  const primitiveType = primitiveTypes.get(type)
+  if (primitiveType) {
+    // eslint-disable-next-line valid-typeof
+    if (typeof value !== primitiveType) {
+      throw new Error(` to be ${primitiveType}. Got: ${typeof value}`)
+    }
+    return
+  }
+  throw new Error(` to have a valid type. Got: ${type}`)
+}
